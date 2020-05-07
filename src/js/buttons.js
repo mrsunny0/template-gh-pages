9ffbb5dbf8455a8a123fdafce196d66d4dd915bd
George Sun <george.lele.sun@gmail.com>
2020-05-07 15:22:21 -0400

    toggling and display works

--- src/js/buttons.js
+++ src/js/buttons.js
@@ -1,17 +1,19 @@
 // find containers and buttons
 var container_grid = document.getElementById('container-grid');
-var container_link = document.getElementById('container-list');
-var button_grid = container_grid.querySelector('.button');
-var button_link = container_link.querySelector('.button');
-var button_footer = document.getElementById('button-footer');
+var container_list = document.getElementById('container-list');
+var checkbox_toggle = document.getElementById("toggle-checkbox");
+// var button_grid = container_grid.querySelector('.button');
+// var button_link = container_list.querySelector('.button');
+// var button_footer = document.getElementById('button-footer');
 
 // get nav wrapper and footer button hrefs
 var nav_a = document.getElementsByTagName('nav')[0]
     .querySelectorAll('a');
-var footer_a = document.getElementById('button-footer')
-    .querySelector('a')
-var a_list = [...nav_a, footer_a]
-a_list.shift()
+// var footer_a = document.getElementById('button-footer')
+//     .querySelector('a')
+// var a_list = [...nav_a, footer_a]
+// a_list.shift()
+var a_list = [...nav_a];
 
 // create two lists of hrefs with original and original+overlay suffix
 var hrefs = a_list.map((a) => {
@@ -23,6 +25,8 @@ var hrefs_overlay = hrefs.map((href) => {
 })
 
 // create function to toggle between original and overlay hrefs
+// this means changing the href from the navigation to the 
+// new -overlay hrefs or vice-versa
 function change_hrefs(bool) {
     if (bool) {
         new_hrefs = hrefs_overlay
@@ -34,43 +38,69 @@ function change_hrefs(bool) {
     })
 }
 
-// add event handlers to buttons
-button_grid.addEventListener('click', (event) => {
-    container_grid.style.display = "none"
-    container_link.style.display = "block"
-    button_grid = 'none'
-    button_link = 'block'
-    change_hrefs(true)
-})
-
-button_link.addEventListener('click', (event) => {
-    container_grid.style.display = "block"
-    container_link.style.display = "none"
-    button_grid = 'block'
-    button_link = 'none'
-    change_hrefs(false)
-})
-
-// change to fixed position once scrolled into
-window.addEventListener('scroll', () => {
-    // innerHeight is 100vh in javascript html viewport meta
-    // grid and link button on/off using opacity changes
-    if (window.pageYOffset >= window.innerHeight * 0.875) {
-        button_grid.style.opacity = 1
-        button_link.style.opacity = 1
-        button_footer.style.opacity = 1
-    } else {
-        // button_grid.style.display = 'none'
-        button_link.style.opacity = 0
-        button_grid.style.opacity = 0
-        button_footer.style.opacity = 0
+// function to control visibility and display of the container
+// block when toggled
+function toggle_display(bool) {
+    // if checked to list view
+    if (bool) {
+        container_grid.style.visibility = "hidden"
+        container_grid.style.display = "none"
+        container_list.style.visibility = "visible"
+        container_list.style.display = "block"
+    } 
+    // default (unchecked) is grid view
+    else {
+        container_grid.style.visibility = "visible"
+        container_grid.style.display = "block"
+        container_list.style.visibility = "hidden"
+        container_list.style.display = "none"
     }
+}
+
+// create function to toggle between grid and list view
+checkbox_toggle.addEventListener("change", function() {
+    toggle_display(this.checked)
 })
+
+container_list.style.display = 'none'
+
+
+// add event handlers to buttons
+// button_grid.addEventListener('click', (event) => {
+//     container_grid.style.display = "none"
+//     container_list.style.display = "block"
+//     button_grid = 'none'
+//     button_link = 'block'
+//     change_hrefs(true)
+// })
+
+// button_link.addEventListener('click', (event) => {
+//     container_grid.style.display = "block"
+//     container_list.style.display = "none"
+//     button_grid = 'block'
+//     button_link = 'none'
+//     change_hrefs(false)
+// })
+
+// // change to fixed position once scrolled into
+// window.addEventListener('scroll', () => {
+//     // innerHeight is 100vh in javascript html viewport meta
+//     // grid and link button on/off using opacity changes
+//     if (window.pageYOffset >= window.innerHeight * 0.875) {
+//         button_grid.style.opacity = 1
+//         button_link.style.opacity = 1
+//         button_footer.style.opacity = 1
+//     } else {
+//         // button_grid.style.display = 'none'
+//         button_link.style.opacity = 0
+//         button_grid.style.opacity = 0
+//         button_footer.style.opacity = 0
+//     }
+// })
 
 // initial link container off and 0 opacity buttons
-container_link.style.display = 'none'
-button_grid.style.opacity = 0
-button_link.style.opacity = 0
-button_footer.style.opacity = 0
+// button_grid.style.opacity = 0
+// button_link.style.opacity = 0
+// button_footer.style.opacity = 0
 
 
