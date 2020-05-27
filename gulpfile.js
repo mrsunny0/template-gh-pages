5e2e17f7a9b5c5372507c1b9c0ac94a097282a54
George Sun <george.lele.sun@gmail.com>
2020-05-26 21:37:12 -0400

    adding initial setup for svg xlink:href

--- gulpfile.js
+++ gulpfile.js
@@ -65,7 +65,7 @@ function css() {
  */
 function images() {
   return gulp
-    .src(['src/img/**/*.{jpg,png,gif}', '!src/img/raw/*', '!src/img/drafts/*'])
+    .src(['src/img/**/*.{jpg,png,gif,svg}', '!src/img/raw/*', '!src/img/drafts/*'])
     .pipe(plumber())
     .pipe(imagemin({
       optimizationLevel: 3,
