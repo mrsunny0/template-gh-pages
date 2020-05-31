0fd3b09ce72534b72d8359d73be75e642a0b1391
George Sun <george.lele.sun@gmail.com>
2020-05-31 10:52:59 -0400

    keeping sweet-scroll

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -4,3 +4,20 @@ document.addEventListener("DOMContentLoaded", function () {
         offset: 0, // in pixels
     });
 }, false)
+
+$(window).scroll(function() {
+    var windscroll = $(window).scrollTop();
+    if (windscroll >= 100) {
+        $('section').each(function(i) {
+            if ($(this).position().top <= windscroll + 84) { // << here '+ 84' instead of '- 20'
+                $('nav a.active').removeClass('active');
+                $('nav a').eq(i).addClass('active');
+            }
+        });
+
+    } else {
+        $('nav a.active').removeClass('active');
+        $('nav a:first').addClass('active');
+    }
+
+}).scroll();
